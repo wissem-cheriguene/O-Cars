5e35e3ad8914b48583b07816dbaa6366c82576f4
vodkorn666 <j.correia34@hotmail.fr>
2021-01-20 15:06:50 +0100

    routes homepage, cgu, about et carsList dans le MainController ok. Lien vers les pages d'accueil et about ok.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="main")
+     * @Route("/", name="homepage")
      */
     public function index(): Response
     {
-        return $this->render('login_form.html.twig', [
+        return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/nous-contactez", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('main/about.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="cgu")
+     */
+    public function cgu(): Response
+    {
+        return $this->render('main/cgu.html.twig',[
+            'controller_name' => 'MainController',
+        ]);
+
+    }
+
+    /**
+     * @Route("/voiture", name="cars_list")
+     */
+    public function carsList(): Response
+    {
+        return $this->render('main/cars_list.html.twig',[
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/voiture/{id}, name="car")
+     */
+    /* 
+    public function car(): Response
+    {
+        return $this->render('main/car.html.twig',[
+            'controller_name' => 'MainController',
+        ]);
+    } 
+    */
 }
