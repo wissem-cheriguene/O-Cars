7761ac47326c05a39a0fd657494a7c29fffae324
WissPress <wissem.cheriguene@gmail.com>
2021-01-22 19:47:00 +0100

    add car in rental entity with car_id OK :muscle:

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Car;
-use App\Repository\BrandRepository;
+use App\Entity\Rental;
+use App\Form\RentalType;
 use App\Repository\CarRepository;
+use App\Repository\BrandRepository;
 use App\Repository\ImagesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
@@ -64,10 +67,35 @@ class MainController extends AbstractController
      * Affichage d'une annonce
      * @Route("/voiture/{id}", name="car", methods={"GET", "POST"})
      */
-    public function car(Car $car): Response
+    public function car(Car $car, Request $request): Response
     {
+        $rental = new Rental();
+        $form = $this->createForm(RentalType::class, $rental);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On récupère les images transmises
+            $data = $form->getData();
+            $rental->setCar($car);
+            // dd($data);
+
+            // faire quelque chose avec l'entité, par exemple la sauvegarder en bdd
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rental);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                'Voiture enregistrée!'
+            );
+
+            // toujours rediriger vers une page après un POST réussi
+            return $this->redirectToRoute('cars_list');
+        } 
+
         return $this->render('main/car.html.twig',[
             'car' => $car,
+            'form' => $form->createView(),
         ]);
-    } 
+    }
 }
---
+++ src/Form/RentalType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Rental;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class RentalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startingDate', DateTimeType::class, [
+                'label' => "Date de début",
+                'widget' => 'single_text',
+                'attr'=> [
+                    'placeholder' => "Du"
+                ],
+            ])
+            ->add('endingDate', DateTimeType::class, [
+                'label' => "Date de fin",
+                'widget' => 'single_text',
+                'attr'=> [
+                    'placeholder' => "Au"
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Rental::class,
+        ]);
+    }
+}
